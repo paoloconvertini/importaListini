df3a22d1563f41bf3d9ac9a5493c4fb123fe1354
paolo.convertini <paolo.convertini@gmail.com>
2022-02-16 17:52:26 +0100

    excel reader

--- src/main/java/it/calolenoci/importaListini/reader/ExcelReader.java
+++ src/main/java/it/calolenoci/importaListini/reader/ExcelReader.java
@@ -1,11 +1,51 @@
 package it.calolenoci.importaListini.reader;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class ExcelReader implements FileReader {
 
     @Override
     public void read(File file) {
+        try {
+            FileInputStream inputStream = new FileInputStream(file);
+            Workbook baeuldungWorkBook = new XSSFWorkbook(inputStream);
+            for (Sheet sheet : baeuldungWorkBook) {
+                int firstRow = sheet.getFirstRowNum();
+                int lastRow = sheet.getLastRowNum();
+                for (int index = firstRow + 1; index <= lastRow; index++) {
+                    Row row = sheet.getRow(index);
+                    for (int cellIndex = row.getFirstCellNum(); cellIndex < row.getLastCellNum(); cellIndex++) {
+                        Cell cell = row.getCell(cellIndex, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+                        printCellValue(cell);
+                    }
+                }
+            }
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public static void printCellValue(Cell cell) {
+        CellType cellType = cell.getCellType().equals(CellType.FORMULA)
+                ? cell.getCachedFormulaResultType() : cell.getCellType();
+        if (cellType.equals(CellType.STRING)) {
+            System.out.print(cell.getStringCellValue() + " | ");
+        }
+        if (cellType.equals(CellType.NUMERIC)) {
+            if (DateUtil.isCellDateFormatted(cell)) {
+                System.out.print(cell.getDateCellValue() + " | ");
+            } else {
+                System.out.print(cell.getNumericCellValue() + " | ");
+            }
+        }
+        if (cellType.equals(CellType.BOOLEAN)) {
+            System.out.print(cell.getBooleanCellValue() + " | ");
+        }
     }
 }
